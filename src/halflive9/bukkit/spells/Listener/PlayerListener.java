038274c832d4befcfec22bfc1d3d09e8a1a8a7bc
Lander Tuyteleers <lander.tuyteleers@gmail.com>
2018-05-24 22:28:37 +0200

    first commit

--- src/halflive9/bukkit/spells/Listener/PlayerListener.java
+++ src/halflive9/bukkit/spells/Listener/PlayerListener.java
@@ -5,6 +5,7 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 import java.util.Set;
@@ -14,8 +15,8 @@ public class PlayerListener implements Listener {
     @EventHandler
     public void onPlayerInteractBlock(PlayerInteractEvent event) {
         Player player = event.getPlayer();
-        if (player.getInventory().getItemInMainHand().getType() == Material.FISHING_ROD) {
-            Bukkit.broadcast("held item!", "");
+        if (player.getInventory().getItemInMainHand().getType() == Material.FISHING_ROD && event.getAction().equals(Action.LEFT_CLICK_AIR)) {
+            player.sendMessage("Lightnigh spawned!");
             player.getWorld().strikeLightning(player.getTargetBlock((Set<Material>) null, 200).getLocation());
         }
     }
